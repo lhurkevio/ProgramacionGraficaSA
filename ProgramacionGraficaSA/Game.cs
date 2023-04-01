06cc805eca4624a7b0cda849acbd5edd717c9557
lhurkevio <carloguzmanlopez5@gmail.com>
2023-04-01 05:55:07 -0400

    Agregar archivos de proyecto.

---
+++ ProgramacionGraficaSA/Game.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace ProgramacionGraficaSA
+{
+    public class Game : GameWindow
+    {
+
+        float[] vertices =
+        {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+        int VertexBufferObject;
+        private Shader shader;
+        int VertexArrayObject;
+
+
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindVertexArray(VertexArrayObject);
+
+
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
+
+
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            shader.Dispose();
+        }
+    }
+}
---
+++ ProgramacionGraficaSA/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenTK.Windowing.Common;
+using System;
+
+namespace ProgramacionGraficaSA
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "CarloGuzmanLopez"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
